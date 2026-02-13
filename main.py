e1fe6ec60cb60587bf4a5cce07ff895f33820701
Karen Valentim <karenvalentim54@gmail.com>
2026-02-13 02:33:24 -0300

    Feat

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from fastapi import FastAPI
 from game.logica import jogar
 from game.esquema import ResultadoJogo
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(
     title="Cassino API",
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
     return {"mensagem": "Cassino rodando 🚀"}
 
-@app.get("/jogar", response_model=ResultadoJogo)
+@app.post("/jogar", response_model=ResultadoJogo)
 def jogar_cassino():
     return jogar()
